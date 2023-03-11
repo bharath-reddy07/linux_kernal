610c1ec1b5c7c7ee9b8982c024cd4653fa9d1fc9
bharath-reddy07 <cricketers213@gmail.com>
2023-03-11 10:21:18 +0530

    Added all program files

---
+++ programs/blank/blank.c
@@ -0,0 +1,13 @@
+#include "peachos.h"
+#include "stdlib.h"
+#include "stdio.h"
+#include "string.h"
+int main(int argc, char** argv)
+{
+    for (int i = 0; i < argc; i++)
+    {
+        printf("%s\n", argv[i]);
+    }
+    
+    return 0;
+}
